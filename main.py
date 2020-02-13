74ee453324a5d350a8a35be22e5409878a0b27d2
rootm0s <rickardmalmkvist@hotmail.com>
2020-02-13 08:21:42 +0100

    updates

--- main.py
+++ main.py
@@ -23,21 +23,11 @@ if __name__ == "__main__":
 	parser.add_argument("-m", "--monitor", action="store_true", help="monitor registry artifacts in real-time and log the results", required=False)
 	parser.add_argument("-q", "--quite", action="store_true", help="hides the console window, logging is still active", required=False)
 	parser.add_argument("-r", "--report", action="store_true", help="parse logs and exported data and generate HTML report", required=False)
-	parser.add_argument("-d", "--debug", action="store_true", help="turn debug on", required=False)
 	args = parser.parse_args()
 
 	""" Hide the console window """
 	if args.quite:
 		utils().hide()
-
-	""" Turn on debugging on all functions """
-	if args.debug:
-		enum().debug()
-		generate().debug()
-		utils().debug()
-		monitor().debug()
-		registry().debug()
-		parse().debug()
 
 	""" Parse through logs and exported data and generate a
 	HTML file using simple tables """
