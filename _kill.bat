8b908030da601c758e3f943058388218f12c49f4
rootm0s <rootm0s@protonmail.com>
2020-02-17 09:47:52 +0100

    Delete _kill.bat

--- _kill.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo [!] Killing Bustr executables, please wait...
-taskkill /f /im bustr.exe
-taskkill /f /im py.exe
-taskkill /f /im python.exe
-taskkill /f /im cmd.exe
-echo [!] Done, press any key to close window
-pause >nul
